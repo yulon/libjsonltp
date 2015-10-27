1b2846a3f239580db4d67403148b52e824cc7e88
Yuchen Wang <yulon@me.com>
2015-10-28 03:04:41 +0800

    Fixed path

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo cp `dirname $0`/inc/*.h /usr/local/include/*.h
-sudo cp `dirname $0`/lib/*.so /usr/local/lib/*.so
+sudo cp `dirname $0`/inc/*.h /usr/local/include/
+sudo cp `dirname $0`/lib/*.so /usr/local/lib/
