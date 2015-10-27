a71de1718432170fba46b62a00f8409ef67c9445
Yuchen Wang <yulon@me.com>
2015-10-28 02:40:26 +0800

    Change path

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo cp `dirname $0`/inc/lowltp.h /usr/include/lowltp.h
-sudo cp `dirname $0`/lib/liblowltp.so /usr/lib/liblowltp.so
+sudo cp `dirname $0`/inc/*.h /usr/local/include/*.h
+sudo cp `dirname $0`/lib/*.so /usr/local/lib/*.so
