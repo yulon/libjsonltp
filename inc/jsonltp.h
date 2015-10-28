626a84f19715340db2994867bd7b1bb9812ff262
Yuchen Wang <yulon@me.com>
2015-10-28 10:52:42 +0800

    Fixed const

--- inc/jsonltp.h
+++ inc/jsonltp.h
@@ -8,8 +8,8 @@ JSONLTP_EXTERN void jsonltp_init(char* dataDir);
 JSONLTP_EXTERN void jsonltp_close();
 JSONLTP_EXTERN int jsonltp(char* line, char* result, int flag);
 
-#define JSONLTP_FLAG_POS = 0x000F;
-#define JSONLTP_FLAG_NER = 0x00F0;
-#define JSONLTP_FLAG_DP = 0x0F00;
-#define JSONLTP_FLAG_SRL = 0xF000;
-#define JSONLTP_FLAG_ALL = 0xFFFF;
+#define JSONLTP_FLAG_POS 0x000F
+#define JSONLTP_FLAG_NER 0x00F0
+#define JSONLTP_FLAG_DP 0x0F00
+#define JSONLTP_FLAG_SRL 0xF000
+#define JSONLTP_FLAG_ALL 0xFFFF
