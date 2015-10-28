a5198947b28c3ff79b4f8e987c9184e3c68ef395
Yuchen Wang <yulon@me.com>
2015-10-28 15:42:41 +0800

    Add SRL, but error

--- src/jsonltp.cpp
+++ src/jsonltp.cpp
@@ -49,32 +49,60 @@ int jsonltp(char* line, char* result, int flag){
 	cJSON_AddItemToObject(jRoot, "words", strs_to_jary(words));
 
 	//Postag
+	vector<string> postags;
 	if (flag & JSONLTP_FLAG_POS) {
-		vector<string> postags;
 		postagger_postag(pos, words, postags);
 		cJSON_AddItemToObject(jRoot, "postags", strs_to_jary(postags));
+	}
 
-		//Named entity recognize
-		if (flag & JSONLTP_FLAG_NER) {
-			vector<string> nes;
-			ner_recognize(ner, words, postags, nes);
-			cJSON_AddItemToObject(jRoot, "nes", strs_to_jary(nes));
+	//NER
+	vector<string> nes;
+	if (flag & JSONLTP_FLAG_NER) {
+		ner_recognize(ner, words, postags, nes);
+		cJSON_AddItemToObject(jRoot, "nes", strs_to_jary(nes));
+	}
+
+	//Parse
+	vector< pair< int, string > > parse;
+	if (flag & JSONLTP_FLAG_DP) {
+		vector<int> heads;
+		vector<string> deprels;
+		parser_parse(par, words, postags, heads, deprels);
+		cJSON* jParses = cJSON_CreateArray();
+		for (int i = 0; i < heads.size(); i++) {
+			parse.push_back(make_pair(heads[i], deprels[i]));
+
+			cJSON* jParse = cJSON_CreateObject();
+			cJSON_AddNumberToObject(jParse, "heads", heads[i]);
+			cJSON_AddStringToObject(jParse, "deprels", deprels[i].c_str());
+			cJSON_AddItemToArray(jParses, jParse);
 		}
+		cJSON_AddItemToObject(jRoot, "parse", jParses);
+	}
 
-		//Parse
-		if (flag & JSONLTP_FLAG_DP) {
-			vector<int> heads;
-			vector<string> deprels;
-			parser_parse(par, words, postags, heads, deprels);
-			cJSON* jParses = cJSON_CreateArray();
-			for (int i = 0; i < heads.size(); i++) {
-				cJSON* jParse = cJSON_CreateObject();
-				cJSON_AddNumberToObject(jParse, "heads", heads[i]);
-				cJSON_AddStringToObject(jParse, "deprels", deprels[i].c_str());
-				cJSON_AddItemToArray(jParses, jParse);
+	//SRL
+	if (flag & JSONLTP_FLAG_SRL) {
+		vector< pair< int, vector< pair<string, pair< int, int > > > > > srl;
+		DoSRL(words, postags, nes, parse, srl);//srl is null?
+		cJSON* jSRL = cJSON_CreateArray();
+		for (int i = 0; i < srl.size(); i++) {
+			cJSON* jPredicate = cJSON_CreateObject();
+			cJSON_AddNumberToObject(jPredicate, "i", srl[i].first);
+
+			cJSON* jArgs = cJSON_CreateArray();
+			for (int j = 0; j < srl[i].second.size(); ++j)
+			{
+				cJSON* jArg = cJSON_CreateObject();
+				cJSON_AddStringToObject(jArg, "type", srl[i].second[j].first.c_str());
+				cJSON_AddNumberToObject(jArg, "beg", srl[i].second[j].second.first);
+				cJSON_AddNumberToObject(jArg, "end", srl[i].second[j].second.second);
+				cJSON_AddItemToArray(jArgs, jArg);
 			}
-			cJSON_AddItemToObject(jRoot, "parse", jParses);
+			cJSON_AddItemToObject(jPredicate, "srl", jArgs);
+
+			cJSON_AddItemToArray(jSRL, jPredicate);
 		}
+		cJSON_AddItemToObject(jRoot, "srl", jSRL);
 	}
 
 	char* jRootStr = cJSON_Print(jRoot);
